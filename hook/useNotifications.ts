234cba5979e33d34b16d68c70e3e9230a0f2b332
japjyot.suri <japjyot.suri@geekyants.com>
2025-11-20 13:35:53 +0530

    feat: #wip Added logic to show notification from native side

--- hook/useNotifications.ts
+++ hook/useNotifications.ts
@@ -1,10 +1,10 @@
-import { NOTIFICATION_COLOR } from "@/constants/Colors";
 import { createNotificationChannel } from "@/utils/notificationChannelUtil";
-import notifee, { AndroidStyle, EventType } from "@notifee/react-native";
+import notifee from "@notifee/react-native";
 import messaging, { FirebaseMessagingTypes } from "@react-native-firebase/messaging";
 import { useRouter } from "expo-router";
 import { useEffect } from "react";
-
+import { DeviceEventEmitter, NativeModules } from 'react-native';
+const { CustomNotification , NotificationClickModule } = NativeModules;
 //Fetching the device's FCM token from Firebase
 const getFCMToken = async () => {
   try {
@@ -27,45 +27,56 @@ const subscribeToTopic = async () => {
 
 async function onMessageReceived(message: FirebaseMessagingTypes.RemoteMessage) {
   try {
-  const channelId = await createNotificationChannel();
+  // const channelId = await createNotificationChannel();
+  console.log("message", message);
   const response = await fetch("https://notifee-mockserver-7yjverznv-japjyotsuris-projects.vercel.app/user");
     const data = await response.json();
     console.log("data", data);
   //Displaying the notification using notifee with fix for showing style in dark mode
-  await notifee.displayNotification({
-    title: message?.data?.title as string || "",
-    android: {
-      channelId, 
-      pressAction: {
-        id: "default",
-      },
-      color: NOTIFICATION_COLOR,
-      colorized: true,
-      timestamp: Date.now(), 
-      showTimestamp: true,   
-      style: {
-        type: AndroidStyle.MESSAGING,
-        person: {
-          name: `${message?.data?.title} ${data?.name || "Jap"}` || "",
-        },
-        messages: [
-          {
-            text: message?.data?.body as string || "",
-            timestamp: Date.now(), 
-          },
+  // await notifee.displayNotification({
+  //   title: message?.data?.title as string || "",
+  //   android: {
+  //     channelId, 
+  //     pressAction: {
+  //       id: "default",
+  //     },
+  //     color: NOTIFICATION_COLOR,
+  //     colorized: true,
+  //     timestamp: Date.now(), 
+  //     showTimestamp: true,   
+  //     style: {
+  //       type: AndroidStyle.MESSAGING,
+  //       person: {
+  //         name: `${message?.data?.title} ${data?.name || "Jap"}` || "",
+  //       },
+  //       messages: [
+  //         {
+  //           text: message?.data?.body as string || "",
+  //           timestamp: Date.now(), 
+  //         },
           
-        ],
-      },
-    },
-    ios: {
-      sound: "default",
-      foregroundPresentationOptions: {
-        alert: true,
-        badge: true,
-        sound: true,
-      },
-    },
-  });
+  //       ],
+  //     },
+  //   },
+  //   ios: {
+  //     sound: "default",
+  //     foregroundPresentationOptions: {
+  //       alert: true,
+  //       badge: true,
+  //       sound: true,
+  //     },
+  //   },
+  // });
+
+  CustomNotification.showCustomNotification(
+    "🚀 Hello <font color='#00FF00'><b>Order Shipped!</b></font>",
+    "Get <font color='#00FF00'><b>50% OFF</b></font> on your next order.<br><i>Limited time only!</i>",
+    "/(tabs)" 
+  );
+  // CustomNotification.showCustomNotification(
+  //   "Hello",
+  //   "how are you?"
+  // );
 } catch (error) {
   console.error('Notification display error:', error);
 }
@@ -88,6 +99,7 @@ const requestUserPermission = async () => {
 
 export default function useNotifications() {
   const router = useRouter();
+
   //Handling the case when the app is launched by tapping a notification from the killed/quit state
   async function initialNotificationHandler() {
     const initialNotification = await notifee.getInitialNotification();
@@ -99,36 +111,83 @@ export default function useNotifications() {
 
   useEffect(() => {
     requestUserPermission();
-    createNotificationChannel();
-    initialNotificationHandler();
+    // createNotificationChannel();
+    console.log("Native modules:", NativeModules);
+    // initialNotificationHandler();
+    // const sub = DeviceEventEmitter.addListener("notificationClicked", (data) => {
+    //   console.log("🔥 Notification clicked:", data);
+    //   try {
+    //     const parsed = JSON.parse(data);
+    //     console.log("🔥 Parsed:", parsed);
+  
+    //     if (parsed.route) {
+    //       router.push(parsed.route);
+    //     }
+    //   } catch (e) {
+    //     console.log("Parse error:", e);
+    //   }
+    // });
+  
+    // const handleDeepLink = (event: { url: string }) => {
+    //   console.log("🔥 Deep link:", event);
+    //   const route = event.url.replace('practicepocs://', '');
+    //   router.push(route as any);
+    // };
+  
+    // // Handle deep link when app is opened
+    // Linking.getInitialURL().then((url) => {
+    //   if (url) handleDeepLink({ url });
+    // });
+  
+    // // Handle deep link when app is already running
+    // const subscription = Linking.addEventListener('url', handleDeepLink);
 
     //Handles FCM messages when the app is running in foreground
+    const sub = DeviceEventEmitter.addListener("notificationClicked", (data) => {
+      console.log("RN received click:", data);
+
+      const route = data.notification_route || data.route;
+      // if (route) router.push(route);
+    });
+
+    // 2. Cold start: app was killed
+    NotificationClickModule.getInitialNotification().then((data: any) => {
+      if (data) {
+        console.log("Cold start notification:", data);
+        const route = data.notification_route || data.route;
+        // if (route) router.push(route);
+      }
+    });
+
     const unsubscribe = messaging().onMessage(onMessageReceived);
 
     // Handles user interactions (presses) on notifications when app is foregrounded
-    const foregroundSubscription = notifee.onForegroundEvent(
-      ({ type, detail }) => {
-        switch (type) {
-          case EventType.PRESS:
-            console.log("User pressed notification");
-            router.push("/(tabs)");
-            break;
-        }
-      }
-    );
+    // const foregroundSubscription = notifee.onForegroundEvent(
+    //   ({ type, detail }) => {
+    //     switch (type) {
+    //       case EventType.PRESS:
+    //         console.log("User pressed notification");
+    //         router.push("/(tabs)");
+    //         break;
+    //     }
+    //   }
+    // );
     // Handles user interactions on notifications when app is in background
-    notifee.onBackgroundEvent(async ({ type, detail }) => {
-      const { notification, pressAction } = detail;
-      if (type === EventType.PRESS) {
-        console.log("notification pressed in background");
-        router.push("/(tabs)");
-        await notifee.cancelNotification(notification?.id ?? "");
-      }
-    });
+    // notifee.onBackgroundEvent(async ({ type, detail }) => {
+    //   const { notification, pressAction } = detail;
+    //   if (type === EventType.PRESS) {
+    //     console.log("notification pressed in background");
+    //     router.push("/(tabs)");
+    //     await notifee.cancelNotification(notification?.id ?? "");
+    //   }
+    // });
     return () => {
       // Cleanup when component unmounts or dependency changes
+      // sub.remove();
+      // subscription.remove();
       unsubscribe();
-      foregroundSubscription();
+      sub.remove();
+      // foregroundSubscription();
     };
   }, [router]);
 
--- index.js
+++ index.js
@@ -1,50 +1,63 @@
-import { NOTIFICATION_COLOR } from "@/constants/Colors";
-import { createNotificationChannel } from "@/utils/notificationChannelUtil";
-import notifee, { AndroidStyle } from "@notifee/react-native";
+import { AppRegistry } from 'react-native';
 import messaging from "@react-native-firebase/messaging";
+import { NativeModules } from 'react-native';
+const { CustomNotification } = NativeModules;
+
+
+AppRegistry.registerHeadlessTask("NotificationEventTask", () =>
+  async (data) => {
+    console.log("Headless notification:", data);
+  }
+);
 
 // runs when we receive a notification when the app is in background or killed state
 messaging().setBackgroundMessageHandler(async (remoteMessage) => {
   console.log("Message handled in the background!", remoteMessage);
-  const channelId = await createNotificationChannel();
+  // const channelId = await createNotificationChannel();
   //Added a mock server to fetch data of user
   const response = await fetch("https://notifee-mockserver-7yjverznv-japjyotsuris-projects.vercel.app/user");
   const data = await response.json();
   console.log("data in index", data);
   // Displaying the notification using notifee with fix for showing style in dark mode
-  await notifee.displayNotification({
-    title: remoteMessage?.data?.title,
-    android: {
-      channelId,
-      pressAction: {
-        id: "default",
-      },
-      color: NOTIFICATION_COLOR,
-      colorized: true,
-      timestamp: Date.now(),
-      showTimestamp: true,
-      style: {
-        type: AndroidStyle.MESSAGING,
-        person: {
-          name: `${remoteMessage?.data?.title} ${data?.name || "Jap"}` || "",
-        },
-        messages: [
-          {
-            text: remoteMessage?.data?.body || "",
-            timestamp: Date.now(),
-          },
-        ],
-      },
-    },
-    ios: {
-      sound: "default",
-      foregroundPresentationOptions: {
-        alert: true,
-        badge: true,
-        sound: true,
-      },
-    },
-  });
+  // await notifee.displayNotification({
+  //   title: remoteMessage?.data?.title,
+  //   android: {
+  //     channelId,
+  //     pressAction: {
+  //       id: "default",
+  //     },
+  //     color: NOTIFICATION_COLOR,
+  //     colorized: true,
+  //     timestamp: Date.now(),
+  //     showTimestamp: true,
+  //     style: {
+  //       type: AndroidStyle.MESSAGING,
+  //       person: {
+  //         name: `${remoteMessage?.data?.title} ${data?.name || "Jap"}` || "",
+  //       },
+  //       messages: [
+  //         {
+  //           text: remoteMessage?.data?.body || "",
+  //           timestamp: Date.now(),
+  //         },
+  //       ],
+  //     },
+  //   },
+  //   ios: {
+  //     sound: "default",
+  //     foregroundPresentationOptions: {
+  //       alert: true,
+  //       badge: true,
+  //       sound: true,
+  //     },
+  //   },
+  // });
+
+  CustomNotification.showCustomNotification(
+    "🚀 Hello <font color='#00FF00'><b>Order Shipped!</b></font>",
+    "Get <font color='#00FF00'><b>50% OFF</b></font> on your next order.<br><i>Limited time only!</i>",
+    "/(tabs)" 
+  );
 });
 
 //Ensuring app navigation is properly initialized
